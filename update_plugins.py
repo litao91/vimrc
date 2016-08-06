5cc10850527d9cb8a0686049ba0da9fd0abecfda
Tao LI <litao912002@hotmail.com>
2016-08-06 11:44:44 +0800

    Remove vim-sync as it doesn't really works

--- update_plugins.py
+++ update_plugins.py
@@ -56,7 +56,6 @@ python-mode https://github.com/klen/python-mode
 django-plug.vim https://github.com/tweekmonster/django-plus.vim.git
 vim-javascript https://github.com/pangloss/vim-javascript
 base16-vim https://github.com/chriskempson/base16-vim
-vim-sync https://github.com/eshion/vim-sync
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
